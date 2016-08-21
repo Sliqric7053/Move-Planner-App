906322e70c1ce28606946ebaf94150a6d278c8b0
Sliqric7053 <isloan@ISLOAN.local>
2016-08-21 20:34:50 +0200

    Add functionality (js) to the page
    Use jQuery to add Google Map API
    Use Ajax to add to the page:
    NY Times articles
    Wikipedia resources

--- js/script.js
+++ js/script.js
@@ -1,21 +1,84 @@
 
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
+    var $body          = $('body');
+    var $wikiElem      = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+    var $nytElem       = $('#nytimes-articles');
+    var $greeting      = $('#greeting');
+    var $street        = $('#street');
+    var $city          = $('#city');
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
+    var apikey = '&key=AIzaSyCzu6WxG9fwKEjct2c-iG2amQgHuf_d2-I';
+    var street = $street.val();
+    var city = $city.val();
+    var address = street + ', ' + city;
 
-    // YOUR CODE GOES HERE!
+    $greeting.text('So you want to live at ' + address.toUpperCase() + '?');
 
-    return false;
+    //setup src
+    var src = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
+    // console.log(src);
+
+    //append <img> to the page
+    $body.append('<img class="bgimg" src= "' + src + '">');
+
+
+
+    //do $.getJSON and add NYT articles
+    var nyTimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + city + '&sort=newest' + apikey;
+    // console.log(nyTimesUrl);
+    $.getJSON(nyTimesUrl, function(data) {
+      // console.log(data);
+      $nytHeaderElem.text('New York Times Articles About: ' + city.toUpperCase());
+
+    articles = data.response.docs;
+    for (var i = 0; i < articles.length; i++) {
+      var article = articles[i];
+      $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+                      '<p>' + article.snippet + '</p>' + '</li>');
+    }
+  }).error(function(err) {
+      $nytHeaderElem.text('New York Times Articles About ' + city.toUpperCase() + ' Could Not Be Loaded');
+
+  });
+
+  //do $.ajax and add wikipedia articles
+  var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + city + '&format=json&callback=wikiCallback';
+  // console.log('wikiUrl: ' + wikiUrl);
+
+  // create a setTimeout to handle errors
+  var wikiRequestTimeout = setTimeout(function() {
+    $wikiElem.text('Failed to get Wikipedia resources! Try again later..')
+  }, 8000);
+
+  $.ajax({
+    url: wikiUrl,
+    dataType: 'jsonp',
+    // jsonp: 'callback', --> explanation: some api require you use a different name for the callback function. But by default, setting dataType: 'jsonp' sets jsonp: 'callback' hence this line parameter is redundant
+    success: function(response) {
+      // console.log('response: ' + response);
+      var articleList = response[1];
+
+      for (var i = 0; i < articleList.length; i++) {
+        articleStr = articleList[i];
+        // console.log('articleStr: ' + articleStr);
+        var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+        // console.log('url: ' + url);
+        $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+      };
+      //clear setTimeout so the $wikiElem does not get overwritten
+      clearTimeout(wikiRequestTimeout);
+    }
+  });
+
+
+  return false;
 };
 
 $('#form-container').submit(loadData);
