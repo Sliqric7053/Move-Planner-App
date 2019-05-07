e592e4d49e68e89a8a45ed2aaeaf551af7d67005
Richard Sithole <richardsit@gmail.com>
2019-05-07 16:37:19 +0200

    Clear page before rendering

--- js/index.js
+++ js/index.js
@@ -5,9 +5,16 @@ window.addEventListener('DOMContentLoaded', main, false);
 function main() {
   const form = document.getElementById('form-container');
   const citySearchTerm = document.getElementById('city');
+  const section = document.getElementById('main');
 
   form.addEventListener('submit', evt => {
     evt.preventDefault();
+
+    // clear html
+    while (section.firstChild) {
+      section.removeChild(section.firstChild);
+    }
+
     const city = citySearchTerm.value;
     const api = new CreateHTML(city);
 
